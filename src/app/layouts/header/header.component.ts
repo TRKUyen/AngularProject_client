49ac63e1413a32dc0f0cf99a0a11b5c1bf0215e9
kishimoto_miya <94293726+TRKUyen@users.noreply.github.com>
2024-04-09 22:19:10 +0700

    fix add module to @Component

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -4,7 +4,7 @@ import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterModule], //fix import module
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
